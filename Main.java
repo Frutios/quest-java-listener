ef5a5ce92c679ef3509a07e9ac9fe76e6cd0a545
Frutios <benjamin.tourneur7@gmail.com>
2020-07-10 00:04:28 +0200

    Observer

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,8 +7,15 @@ public class Main {
         Bank account = new Bank(1000);
 
         // TODO : create and use a TransactionListener interface with onComplete method
-        Transactions.withdraw(account, 100);
 
-        System.out.printf("Your balance is: %d%n", account.getTotal());
+
+        Transactions.withdraw(account, 100, new TransactionListener(){
+            @Override
+            public void onComplete(Bank account) {
+                System.out.printf("Your balance is: %d%n", account.getTotal());
+                
+            }
+        });
+
     }
 }
